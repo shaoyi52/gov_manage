39330fe3cb70d67f5cd9fe64ac80e10829a4fcbd
shaoyi52 <824132231@qq.com>
2019-08-22 22:41:18 +0800

    create

---
+++ src/components/page/Login.vue
@@ -0,0 +1,194 @@
+<template>
+    <div class="login-wrap">
+        <div class="ms-header">晋江智慧旅游广告管理平台</div>
+        <div class="ms-login">
+            <div class="ms-title">后台管理系统</div>
+            <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="ms-content">
+                <el-form-item prop="username">
+                    <el-input v-model="ruleForm.username" placeholder="username">
+                        <el-button slot="prepend" icon="el-icon-lx-people"></el-button>
+                    </el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input type="password" placeholder="password" v-model="ruleForm.password" @keyup.enter.native="submitForm('ruleForm')">
+                        <el-button slot="prepend" icon="el-icon-lx-lock"></el-button>
+                    </el-input>
+                </el-form-item>
+                <div class="login-btn">
+                    <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+                </div>
+                <p class="login-tips">Tips : 用户名和密码随便填。</p>
+            </el-form>
+        </div>
+        <div class="ms-login-wrap">
+            <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="ms-login-content">
+                <el-form-item prop="username">
+                    <el-input v-model="ruleForm.username" placeholder="username">
+                        <el-button slot="prepend" icon="el-icon-lx-people"></el-button>
+                    </el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input type="password" placeholder="password" v-model="ruleForm.password" @keyup.enter.native="submitForm('ruleForm')">
+                        <el-button slot="prepend" icon="el-icon-lx-lock"></el-button>
+                    </el-input>
+                </el-form-item>
+                <div class="login-btn">
+                    <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+                </div>
+                <p class="login-tips">Tips : 用户名和密码随便填。</p>
+            </el-form>
+
+            
+           
+        </div>
+        <div class="ms-footer">copyright ©2019-2020 隆升信息 版权所有</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function(){
+            return {
+                ruleForm: {
+                    username: 'admin',
+                    password: '123123'
+                },
+                rules: {
+                    username: [
+                        { required: true, message: '请输入用户名', trigger: 'blur' }
+                    ],
+                    password: [
+                        { required: true, message: '请输入密码', trigger: 'blur' }
+                    ]
+                }
+            }
+        },
+        methods: {
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        localStorage.setItem('ms_username',this.ruleForm.username);
+                        this.$router.push('/');
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            }
+        }
+    }
+</script>
+<style rel="stylesheet/scss" lang="scss"  >
+    .login_input{
+        input{
+            background: none;
+            border: none;
+        }
+    }  
+    .login-wrap{
+        .el-button--primary {
+             background: #3a8ee6;
+            border-color: #3a8ee6;
+            color: #FFF;
+            
+        }
+        .el-button--primary:active,.el-button--primary:hover{
+           color: #FFF;
+           background-color: #409EFF;
+           border-color: #409EFF;
+        }
+    }
+</style>
+<style rel="stylesheet/scss" lang="scss"  scoped >
+    .login-wrap{
+        position: relative;
+        width:100%;
+        height:100%;
+        background-image: url(../../assets/img/login_bg.jpg);
+        background-size: cover;
+        min-width: 1024px;
+        min-height: 768px;
+    }
+    
+    .ms-header{
+        letter-spacing:1em;
+        text-align: center;
+        font-size:24px;
+        color: #fff;
+        padding-top:120px;
+    }
+    .ms-footer{
+        position:absolute;
+        left:0;
+        bottom:80px;
+        font-size: 12px;
+        color: #fff;
+        text-align: center;
+        width: 100%;
+    }
+    .ms-login-wrap{
+        position: absolute;
+        right:140px;
+        top:50%;
+        margin-top:-193px;
+        background-image: url(../../assets/img/login-form-bg.png);
+        background-size: cover;
+        width:420px;
+        height: 386px;   
+          
+    }
+    .ms-login-content{
+        padding: 140px 30px;
+    }
+    
+    .userRowBg{
+        width: 346px;
+        height: 49px;
+        background-image: url(../../assets/img/userRowBg.png);
+        background-size: cover;
+    }
+
+    .pwRowBg{
+        width: 346px;
+        height: 49px;
+        background-image: url(../../assets/img/pwRowBg.png);
+        background-size: cover;
+    }
+    
+    .ms-title{
+        width:100%;
+        line-height: 50px;
+        text-align: center;
+        font-size:20px;
+        color: #fff;
+        border-bottom: 1px solid #ddd;
+    }
+    .ms-login{
+        display:none;
+        position: absolute;
+        left:50%;
+        top:50%;
+        width:350px;
+        margin:-190px 0 0 -175px;
+        border-radius: 5px;
+        background: rgba(255,255,255, 0.3);
+        overflow: hidden;
+    }
+    
+    .ms-content{
+        padding: 30px 30px;
+    }
+    .login-btn{
+        text-align: center;
+    }
+    .login-btn button{
+        width:100%;
+        height:36px;
+        margin-bottom: 10px;
+    }
+    .login-tips{
+        font-size:12px;
+        line-height:30px;
+        color:#fff;
+    }
+</style>
---
+++ src/components/page/Scenic.vue
@@ -0,0 +1,176 @@
+<template>
+    <div>
+        <el-row :gutter="20">
+            <el-col :span="8">
+                <el-card shadow="hover" class="mgb20" style="height:185px;"> 
+                    <el-row :gutter="40">
+                        <el-col :span="12">
+                            <div class="scenery-des">
+                                <div class="scenery-img"><img  src="../../assets/img/img.jpg"  alt="" class="scenery-img"></div>
+                                <div class="scenery-name"> 清源山</div>
+                            </div>
+                        </el-col>
+                        <el-col :span="12">
+                            <div class="scenery-info">
+                                <div class="scenery-info-row">景区等级：<span>2018-01-01</span></div>
+                                <div class="scenery-info-row">开机时间：<span>8:00-18:00</span></div>
+                                <div class="scenery-info-row">昨日刷证客流：<span>3899次</span></div>
+                                <div class="scenery-info-row">投放设备数：<span>5台</span></div>
+                            </div>
+                        </el-col>
+                    </el-row> 
+                </el-card> 
+            </el-col>
+        </el-row>
+        
+    </div>
+</template>
+
+<script>
+    import Schart from 'vue-schart';
+    import bus from '../common/bus';
+    export default {
+        name: 'dashboard',
+        data() {
+            return {
+                name: localStorage.getItem('ms_username'),
+                todoList: [{
+                        title: '今天要修复100个bug',
+                        status: false,
+                    },
+                    {
+                        title: '今天要修复100个bug',
+                        status: false,
+                    },
+                    {
+                        title: '今天要写100行代码加几个bug吧',
+                        status: false,
+                    }, {
+                        title: '今天要修复100个bug',
+                        status: false,
+                    },
+                    {
+                        title: '今天要修复100个bug',
+                        status: true,
+                    },
+                    {
+                        title: '今天要写100行代码加几个bug吧',
+                        status: true,
+                    }
+                ],
+                data: [{
+                        name: '2018/09/04',
+                        value: 1083
+                    },
+                    {
+                        name: '2018/09/05',
+                        value: 941
+                    },
+                    {
+                        name: '2018/09/06',
+                        value: 1139
+                    },
+                    {
+                        name: '2018/09/07',
+                        value: 816
+                    },
+                    {
+                        name: '2018/09/08',
+                        value: 327
+                    },
+                    {
+                        name: '2018/09/09',
+                        value: 228
+                    },
+                    {
+                        name: '2018/09/10',
+                        value: 1065
+                    }
+                ],
+                options: {
+                    title: '最近七天每天的用户访问量',
+                    showValue: false,
+                    fillColor: 'rgb(45, 140, 240)',
+                    bottomPadding: 30,
+                    topPadding: 30
+                },
+                options2: {
+                    title: '最近七天用户访问趋势',
+                    fillColor: '#FC6FA1',
+                    axisColor: '#008ACD',
+                    contentColor: '#EEEEEE',
+                    bgColor: '#F5F8FD',
+                    bottomPadding: 30,
+                    topPadding: 30
+                }
+            }
+        },
+        components: {
+            Schart
+        },
+        computed: {
+            role() {
+                return this.name === 'admin' ? '超级管理员' : '普通用户';
+            }
+        },
+        created(){
+            this.handleListener();
+            this.changeDate();
+        },
+        activated(){
+            this.handleListener();
+        },
+        deactivated(){
+            window.removeEventListener('resize', this.renderChart);
+            bus.$off('collapse', this.handleBus);
+        },
+        methods: {
+            changeDate(){
+                const now = new Date().getTime();
+                this.data.forEach((item, index) => {
+                    const date = new Date(now - (6 - index) * 86400000);
+                    item.name = `${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()}`
+                })
+            },
+            handleListener(){
+                bus.$on('collapse', this.handleBus);
+                // 调用renderChart方法对图表进行重新渲染
+                window.addEventListener('resize', this.renderChart)
+            },
+            handleBus(msg){
+                setTimeout(() => {
+                    this.renderChart()
+                }, 300);
+            },
+            renderChart(){
+                this.$refs.bar.renderChart();
+                this.$refs.line.renderChart();
+            }
+        }
+    }
+
+</script>
+
+
+<style scoped>
+    .el-row {
+        margin-bottom: 20px;
+    }
+    .scenery-name{
+        text-align: center;
+        padding: 8px 0;
+        color:#606266;
+    }
+    .scenery-img{
+        border-radius: 10%;
+        width: auto;
+        max-height: 175px;
+    }
+   .scenery-info-row{
+       font-size: 14px;
+       padding: 8px 0;
+       color:#606266;
+   }
+ 
+
+</style>
